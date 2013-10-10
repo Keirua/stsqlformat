29575adca29d3420e572a2d1ac7f3ff29db6f823
Keirua <keirua@gmail.com>
2013-10-10 23:23:19 +0200

    it's alive

---
+++ README.md
@@ -0,0 +1,3 @@
+# format_sql
+
+Added possibility to format SQL queries in sublime text 3 through the 'format_sql' command
---
+++ format_sql.py
@@ -0,0 +1,18 @@
+import sublime
+import sublime_plugin
+import sqlparse
+
+class FormatSqlCommand(sublime_plugin.TextCommand):  
+    def run(self, edit):
+        settings = self.view.settings()
+        indent_size = int(settings.get("tab_size"))
+        for s in self.view.sel():
+            txt = format(
+                self.view.substr(s).encode("utf-8"),
+                keyword_case="upper",
+                identifier_case="lower",
+                reindent=True,
+                indent_width=indent_size
+            )
+
+            self.view.replace(edit, s, txt)
